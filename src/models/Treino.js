4c7684710001f90fe89bb7217c18feb8528c0f46
Luiza Rolfsen <rolfsenluiza@email.com>
2026-06-11 17:38:06 -0300

    fix: corrige duplicacao em Treino.js e index.js apos merge; feat: adiciona AuthService, AuthController e rotas de autenticacao

--- src/models/Treino.js
+++ src/models/Treino.js
@@ -27,29 +27,4 @@ const Treino = sequelize.define('Treino', {
   timestamps: true,
 })
 
-export default Treinoimport { DataTypes } from 'sequelize'
-import sequelize from '../database/connection.js'
-
-const Treino = sequelize.define('Treino', {
-  id: {
-    type: DataTypes.INTEGER,
-    primaryKey: true,
-    autoIncrement: true,
-  },
-  aluno_id: {
-    type: DataTypes.INTEGER,
-    allowNull: false,
-  },
-  nome: {
-    type: DataTypes.STRING(50),
-    allowNull: false,
-  },
-  descricao: {
-    type: DataTypes.TEXT,
-  },
-}, {
-  tableName: 'treinos',
-  timestamps: true,
-})
-
 export default Treino
